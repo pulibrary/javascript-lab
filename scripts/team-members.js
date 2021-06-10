21b5572fb9b5f9489caeaaa7316093c1dbc6012d
Thanya Begum <tbegum@princeton.edu>
2021-06-10 10:19:15 -0400

    Populate team members array, add images

---
+++ scripts/team-members.js
@@ -0,0 +1,40 @@
+/* Bios source: https://www.thrillist.com/entertainment/nation/best-tv-characters-of-the-21st-century-ranked */
+
+const teamMembers = [
+    {
+        name: 'Dewey Wilkerson',
+        role: 'Little Brother',
+        photoFileName: 'dewey.jpeg',
+        bio: "How perfect Dewey was as both the kid brother to his rowdy elder siblings, especially Malcolm and Jamie, who constantly made him the butt of their sometimes cruel pranks, and as the baby of the family to Lois and Hal. He was always armed with a spot-on innocently irreverent punchline, something sorely lacking in the primetime sitcom realm of the early 2000s."
+    },
+    {
+        name: 'Gregory House',
+        role: 'Doctor',
+        photoFileName: 'house.jpeg',
+        bio: "Cantankerous, curmudgeon, another c-word that isn't fit to print: These things all describe Dr. Gregory House, the character English actor Hugh Laurie embodied over medical drama House's eight seasons, earning him six Emmy nods. But he was the Sherlock Holmes of the medical world, a sleuth-like, pill-popping genius picking up on details that others glossed over to arrive at a rare patient diagnosis to the ire of the doctors around them."
+    },
+    {
+        name: 'Marceline',
+        role: 'Vampire Queen',
+        photoFileName: 'marceline.jpeg',
+        bio: "Introduced early on with a smooth jazzy bass-strummed tune about her soul-sucking dad eating her fries, Marceline is a pariah in this mysterious post-apocalyptic world, and both as the series' 10 seasons wore on and in the first of the Adventure Time 'miniseries' Stakes, we saw more and more of the devastating things that happened to her in the before-times that led to her vampirism, her entwined past with the Ice King, and her romantic entanglement with Princess Bubblegum."
+    },
+    {
+        name: 'Olivia Dunham',
+        role: 'FBI Agent',
+        photoFileName: 'olivia.jpeg',
+        bio: "It takes a lot to be a female FBI agent in a show about the government examining paranormal occurrences and not become a clone of the OG paranormal FBI agent Dana Scully, but Olivia Dunham, the complex and fascinating protagonist of the equally complex and fascinating Fringe, carved out a space for herself in the pantheon. Her arc on the show as she comes to terms with her inner trauma and her own identity -- especially when, spoiler alert, she meets an alternate version of herself in the later seasons -- is even more compelling than all the weird stuff encountered by the Fringe Division."
+    },
+    {
+        name: 'Jack',
+        role: 'Samurai',
+        photoFileName: 'jack.jpeg',
+        bio: "The nameless hero warrior known throughout Earth's dystopian future as, simply, 'Jack,' clad in samurai robes and carrying a magic sword, is so cool. Jack, who was zapped from feudal Japan into the far future by the evil demon Aku just before he could land the finishing blow, spends hardly any time worrying about his predicament, and almost immediately starts working on a plan to get back to his home in the past and defeat Aku for good."
+    },
+    {
+        name: 'Holt',
+        role: 'Captain',
+        photoFileName: 'holt.jpeg',
+        bio: "Holt is the no-nonsense boss of the 99, meant to be a foil to Andy Samberg's goofy Jake Peralta. It could be a boring dynamic, but creators Dan Goor and Mike Schur wrote Holt to defy expectations at every turn. He's a gay man who loves his corgi Cheddar, the music of John Philip Sousa, and puns delivered dryly with a straight face. Far from being just a series of discordant traits, Holt is a fully realized human whose life and career has defied the odds of prejudice and who delivers dialogue with a positively Shakespearean flair."
+    }
+]
