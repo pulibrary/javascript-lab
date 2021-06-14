9ef7e7417514cf0cc556122e779449d9b7eb445b
Thanya Begum <tbegum@princeton.edu>
2021-06-14 17:34:49 -0400

    Delete unnecessary comments, stylesheets

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,35 +1,3 @@
-// // Updates the modal's contents based on the member that was clicked
-// function updateModal(member) {    
-//     modalLabel.textContent = member.name;
-//     modalImg.src = `../images/${member.photoFileName}`;
-//     modalImg.alt = `${member.name}`;
-//     modalTitle.textContent = member.name;
-//     modalSubtitle.textContent = member.role;
-//     modalBody.textContent = member.bio;
-// }
-
-// // When the user clicks on a team member's name/photo, open and update the modal 
-// photoGrid.addEventListener('click', event => {
-//     if (event.target.className === "photo" || event.target.className === "name") {
-//         console.log(event)
-//         const member = teamMembers.find(element => element.name === event.target.title);
-//         updateModal(member);
-//         modal.style.display = "block";
-//     }
-// });
-
-// // When the user clicks on Close (x), close the modal
-// closeBtn.addEventListener('click', () => {
-//     modal.style.display = "none";
-// });
-
-// // When the user clicks anywhere outside of the modal, close the modal
-// window.addEventListener('click', event => {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// });
-
 let modal = document.getElementById('modal'); // The modal
 
 // When the user clicks on a team member's card, open and update the modal 
